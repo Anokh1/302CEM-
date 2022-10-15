d9b1c8ddef5a47d7d318c8d36d0a530265c32477
Adrian00I <aokhadrian@gmail.com>
2022-10-16 01:25:40 +0800

    Converted HTML pages to PHP pages and tried connecting SQL database

---
+++ Laptop/Template/_banner-ads.php
@@ -0,0 +1,8 @@
+<!-- Banner Ads -->
+    <section id="banner_ads">
+        <div class="container py-5 text-center">
+            <img src="./assets/adbanner1.PNG" alt="adbanner1" class="img-fluid">
+            <img src="./assets/adbanner2.PNG" alt="adbanner2" class="img-fluid">
+        </div>
+    </section>
+<!-- !Banner Ads -->
